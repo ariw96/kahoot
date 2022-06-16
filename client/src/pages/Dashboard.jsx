22c027fba4b11c97e96b0edee18e2bd1688610b7
ariw96 <ariw96@gmail.com>
2022-06-16 14:48:52 +0300

    deploy 1

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -24,7 +24,7 @@ function Dashboard() {
 		return () => {
 			dispatch(reset());
 		};
-	}, [user, navigate, dispatch, isError, message]);
+	}, [user, navigate, dispatch]);
 	if (isLoading) {
 		return <Spinner />;
 	}
